9fdbe2b253b385b059cfab3802f35e31e75aca0d
marcoramos17 <marcoandreramos17@gmail.com>
2024-08-04 00:21:54 +0100

    Documentation testing

--- src/test_mod.rs
+++ src/test_mod.rs
@@ -1,3 +1,15 @@
+/// This function prints Hello World
+/// # Arguments
+/// * s
+/// * a
+///
+/// # Example
+/// ```
+/// let s = 10
+/// let a = 2
+/// print(a+s)
+/// ```
 pub fn hello() {
+    ///This gives us the 'hello world'
     println!("Hello World!");
 }
