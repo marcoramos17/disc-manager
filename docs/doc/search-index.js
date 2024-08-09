209365c98567bebd77205603eb0992374c7afd13
marcoramos17 <marcoandreramos17@gmail.com>
2024-08-09 19:12:23 +0100

    Documentation testing

---
+++ docs/doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["DiskManagement",{"doc":"","t":"HCH","n":["main","test_mod","hello"],"q":[[0,"DiskManagement"],[2,"DiskManagement::test_mod"]],"d":["","","This function prints Hello World"],"i":[0,0,0],"f":"{{}b}`0","c":[],"p":[[1,"unit"]],"b":[]}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
---
+++ docs/doc/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["DiskManagement",["",[],["main.rs","test_mod.rs"]]]\
+]'));
+createSrcSidebar();
